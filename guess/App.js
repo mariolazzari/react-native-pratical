e8db1763c8a22c94d4db9574d1fc5826e2629339
Mario Lazzari <mario.lazzari@gmail.com>
2021-05-24 19:11:28 +0200

    Done lesson 59

--- guess/App.js
+++ guess/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
       <StartGameScreen />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
 });
+
+export default App;
--- guess/screens/StartGameScreen.js
+++ guess/screens/StartGameScreen.js
@@ -1,43 +1,45 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Start New Game</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a number</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" />
-          <Button title="Confirm" />
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    paddingTop: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
-    marginVertical: 10,
+    marginVertical: 10
   },
   inputContainer: {
     width: 300,
-    maxWidth: "80%",
-    alignItems: "center",
+    maxWidth: '80%',
+    alignItems: 'center'
   },
   buttonContainer: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    paddingHorizontal: 15,
-  },
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
+  }
 });
 
 export default StartGameScreen;
