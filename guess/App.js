e19ec821085802c6d329573fb118d509d10a54e2
Mario Lazzari <mario.lazzari@gmail.com>
2021-05-25 21:35:56 +0200

    Done lesson 63

--- guess/App.js
+++ guess/App.js
@@ -4,19 +4,17 @@ import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 
-const App = () => {
+export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
       <StartGameScreen />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
 });
-
-export default App;
