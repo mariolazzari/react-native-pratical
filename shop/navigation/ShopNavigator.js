77bf2b6df93b94163d6f8d9b0ceb8ecf075cdc3f
Mario Lazzari <mario.lazzari@gmail.com>
2021-06-17 22:06:02 +0200

    Done lesson 171

--- shop/navigation/ShopNavigator.js
+++ shop/navigation/ShopNavigator.js
@@ -1,22 +1,28 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { Platform } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
 
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
-import Colors from '../constants/Colors';
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen
+    ProductDetail: ProductDetailScreen,
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
       },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold",
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+    },
   }
 );
 
--- shop/screens/shop/ProductsOverviewScreen.js
+++ shop/screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { FlatList } from "react-native";
+import { useSelector } from "react-redux";
 
-import ProductItem from '../../components/shop/ProductItem';
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
@@ -16,9 +16,9 @@ const ProductsOverviewScreen = props => {
           title={itemData.item.title}
           price={itemData.item.price}
           onViewDetail={() => {
-            props.navigation.navigate('ProductDetail', {
+            props.navigation.navigate("ProductDetail", {
               productId: itemData.item.id,
-              productTitle: itemData.item.title
+              productTitle: itemData.item.title,
             });
           }}
           onAddToCart={() => {}}
@@ -29,7 +29,7 @@ const ProductsOverviewScreen = props => {
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products'
+  headerTitle: "All Products",
 };
 
 export default ProductsOverviewScreen;
